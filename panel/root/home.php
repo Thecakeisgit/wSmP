2ab1215d169720043235f68f6d29492e964e53d2
Thecakeisgit <Thecakeisplaying@gmail.com>
2016-11-26 00:21:48 +0100

    Added FontAwesome link for icons

--- panel/root/home.php
+++ panel/root/home.php
@@ -5,13 +5,13 @@
     <title>wSmP :: Home</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
-    <link rel="icon" type="image/svg+xml" href="assets/imgs/favicon.svg" sizes="96x96">
 
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/home.css">
   </head>
   <body>
     <div class="menu">
-      <li><a class="menuRight" href="./index.php?page=logout">Logout</a></li>
+      <li><a class="menuRight" href="./index.php?page=logout"><i class="fa fa-power-off"></i> Logout</a></li>
     </div>
   </body>
 </html>
